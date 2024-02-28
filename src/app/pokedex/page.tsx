322d3260bb1eb97bb7ac3f3ea1c0a182da4f507b
Hector Jimenez <xmyoot@yahoo.com>
2024-02-27 18:57:11 -0800

    created Pokedex page with Main Display and Side Display Components

---
+++ src/app/pokedex/page.tsx
@@ -0,0 +1,21 @@
+import MainDisplay from '@/components/MainDisplay';
+import SideDisplay from '@/components/SideDisplay';
+// async function getData() {
+//   const res = await fetch('https://pokeapi.co/api/v2/pokemon/');
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data');
+//   }
+//   return res.json();
+// }
+
+const Pokedex = async () => {
+  // const data = await getData();
+  // const listUrl = data.results.map((pokemon: any) => pokemon.url);
+  return (
+    <main className="flex min-h-screen flex-row content-end justify-center p-5">
+      <MainDisplay />
+      <SideDisplay />
+    </main>
+  );
+};
+export default Pokedex;
